f86ab2c9ca75275ad294d49c796c8f43bcdbe105
Byron Lara <byronlara5@gmail.com>
2016-09-30 14:06:46 -0400

    Changes in develop

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
 from import_export.admin import ImportExportMixin, ExportActionModelAdmin 
-from tracker.models import opeDuquesa
+from tracker.models import opeDuquesa, Camion, Compania
 
 # Register your models here.
 
 class opeDuquesaAdmin(ImportExportMixin, admin.ModelAdmin):
     list_filter = ['date']
 
+class CamionesAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_filter = ['ficha']
+
 
 admin.site.register(opeDuquesa, opeDuquesaAdmin)
+admin.site.register(Camion, CamionesAdmin)
+admin.site.register(Compania)
--- tracker/models.py
+++ tracker/models.py
@@ -4,12 +4,36 @@ from django.db import models
 
 # Create your models here.
 
+
+class Camion(models.Model):
+	ficha = models.CharField(max_length=20, unique=True, primary_key=True)
+	tipo_camion = models.CharField(max_length=20)
+	capacidad = models.IntegerField()
+
+	def __unicode__(self):
+		return u'%s' % (self.ficha)
+
+		
 class opeDuquesa(models.Model):
 	id = models.AutoField(primary_key=True)
 	date = models.DateField()
-	ficha_camion = models.CharField(max_length=15)
+	ficha_camion = models.ForeignKey(Camion)
 	circunscripcion = models.CharField(max_length=2)
 	ton = models.IntegerField()
 
 	def __unicode__(self):
-		return u'%s' % (self.date) 
+		return u'%s' % (self.date)
+
+class Compania(models.Model):
+	nombre = models.CharField(max_length=20)
+	camiones = models.ManyToManyField(Camion)
+
+	def __unicode__(self):
+		return u'%s' % (self.nombre)
+
+
+
+
+
+
+
